537ed84197455e9394bcec8653af038f02583ef6
Tomas Kubica <tkubica12@gmail.com>
2022-02-10 21:29:23 +0100

    fix dns zone reference

--- environments/ephemeral/main.tf
+++ environments/ephemeral/main.tf
@@ -7,8 +7,8 @@ module "main" {
   vnetResourceGroup       = azurerm_resource_group.test.name
   subnetPrefixAks         = "10.0.0.0/22"
   subnetPrefixServices    = "10.0.4.0/24"
-  privateDnsZoneIdAks     = azurerm_private_dns_zone.sql.id
-  privateDnsZoneIdSql     = azurerm_private_dns_zone.aks.id
+  privateDnsZoneIdAks     = azurerm_private_dns_zone.aks.id
+  privateDnsZoneIdSql     = azurerm_private_dns_zone.sql.id
   logAnalyticsWorkspaceId = azurerm_log_analytics_workspace.test.id
   depends_on = [
     azurerm_virtual_network.test,
