76471b53b4be7fd1ed42c67850d61d8e294a45f8
Tomas Kubica <tkubica12@gmail.com>
2022-02-10 21:13:05 +0100

    remove duplicate subnet

--- environments/ephemeral/ephemeral.tf
+++ environments/ephemeral/ephemeral.tf
@@ -11,22 +11,6 @@ resource "azurerm_virtual_network" "test" {
   address_space       = ["10.0.0.0/16"]
 }
 
-resource "azurerm_subnet" "aks" {
-  name                                           = "aks-subnet"
-  resource_group_name                            = azurerm_resource_group.test.name
-  virtual_network_name                           = azurerm_virtual_network.test.name
-  address_prefixes                               = ["10.0.0.0/20"]
-  enforce_private_link_endpoint_network_policies = true
-}
-
-resource "azurerm_subnet" "services" {
-  name                                           = "services-subnet"
-  resource_group_name                            = azurerm_resource_group.test.name
-  virtual_network_name                           = azurerm_virtual_network.test.name
-  address_prefixes                               = ["10.0.4.0/24"]
-  enforce_private_link_endpoint_network_policies = true
-}
-
 // DNS zone
 resource "azurerm_private_dns_zone" "sql" {
   name                = "privatelink.database.windows.net"
