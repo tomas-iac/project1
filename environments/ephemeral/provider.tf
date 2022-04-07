976070d1c179d84b5e06714d114fb244ac701a11
Tomas Kubica <tkubica12@gmail.com>
2022-04-07 11:22:42 +0200

    remove reference to provider state

--- environments/ephemeral/provider.tf
+++ environments/ephemeral/provider.tf
@@ -6,13 +6,6 @@ terraform {
     }
   }
 
-  backend "azurerm" {
-    resource_group_name  = "shared-services"
-    storage_account_name = "tomasiac"
-    container_name       = "tfstate"
-    key                  = "project1-stage.tfstate"
-    subscription_id      = "a0f4a733-4fce-4d49-b8a8-d30541fc1b45"
-  }
 }
 
 provider "azurerm" {
